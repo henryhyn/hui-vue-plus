cedd8947547214dfa31bf6ca06ac1e2e1f47f163
Henry He <henryhyn@163.com>
2024-07-02 11:57:24 +0800

    feat: list style

---
+++ docs/examples/list/demo1.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+ul.list-unstyled
+  li(v-for='i in 3') Item {{ i }}
+ul.list-inline
+  li(v-for='i in 3') Item {{ i }}
+ul.list-inline.separated.by-gt
+  li(v-for='i in 3') Item {{ i }}
+ul.list-inline.separated.by-middot
+  li(v-for='i in 3') Item {{ i }}
+</template>
---
+++ docs/style/list.md
@@ -0,0 +1,5 @@
+# List 列表
+
+:::preview
+demo-preview=../examples/list/demo1.vue
+:::
