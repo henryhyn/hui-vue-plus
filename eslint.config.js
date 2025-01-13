687c781a98a8c466a16c8a373e5d08fba325ebf2
Henry He <henryhyn@163.com>
2025-01-13 23:39:10 +0800

    feat: pre-commit hook

--- eslint.config.js
+++ eslint.config.js
@@ -4,16 +4,25 @@ import pluginVue from 'eslint-plugin-vue';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['**/dist', '**/cache'] },
-  { files: ['**/*.{js,mjs,cjs,vue}'] },
+  { ignores: ['**/{dist,cache}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest'
+      }
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off'
+    }
+  },
+  {
     rules: {
       'no-undef': 'off',
-      'no-unused-vars': 'off',
-      'vue/multi-word-component-names': 'off'
+      'no-unused-vars': 'off'
     }
   }
 ];
